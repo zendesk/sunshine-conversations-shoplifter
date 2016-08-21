67086c40154fdddc0b92f95392d2bdbe3b1eb427
Andrew Lavers <andrew@smooch.io>
2016-08-21 14:24:43 -0400

    Add error view

--- server.js
+++ server.js
@@ -15,17 +15,29 @@ app.use(express.static('public'));
 app.use(expressLayouts);
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.renderMain = (view, props) => {
+        props = props || {};
+        props.layout = 'main';
+        res.render(view, props);
+    }
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.render('addToSmooch', Object.assign(config, {
-        layout: 'main'
-    }));
+    res.renderMain('addToSmooch', config);
 });
 
 app.get('/oauth', (req, res) => {
-    res.render('exchangeToken', Object.assign(config, {
-        layout: 'main',
-        code: req.query.code
-    }));
+    if (req.query.error) {
+        return res.renderMain('error', {
+            error: req.query.error
+        });
+    } else {
+        const props = config;
+        props.code = req.query.code;
+        return res.render('exchangeToken', props);
+    }
 })
 
 app.listen(process.env.PORT || 3000);

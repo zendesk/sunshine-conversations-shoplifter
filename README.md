7f9143321601a6e6c8c6191b695baf71a561a9a4
jugarrit <julian@smooch.io>
2016-09-29 13:48:36 -0400

    Update demo to add readme, settings and success pages (#2)
    * Update demo to add readme, settings and success pages
    
    * Update release notes with configuration options
    
    * Add better configuration options
    
    * Remove jet-simple
    
    * Fix dependencies
    
    * Fix jwt decode
    
    * Fix smooch button
    
    * Remove all the fun
    
    * More UI tweaks

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Shoplifter
 
-A refrence implementation of Smooch.io OAuth flows. A working example can be found at https://shoplifter.herokuapp.com.
+A reference implementation of Smooch.io OAuth flows. A working example can be found at https://shoplifter.herokuapp.com.
+
+# How to use
+
+1. Install node modules: `npm install`
+1. Run the server `node server.js`
+
+# Configuration
+
+Set the following environment variables to configure the app to target your OAuth Client:
+
+```bash
+set CLIENT_ID <your_client_id>
+set REDIRECT_URL <your_redirect_url>
+set SECRET <your_secret>
+```
+
+```bash
+set NODE_ENV production
+```
--- config.js
+++ config.js
@@ -1,33 +1,24 @@
 const _ = require('lodash');
 
 let config = {
-    smoochBaseUrl: process.env.SMOOCH_BASE_URL,
-    clientId: process.env.CLIENT_ID,
-    redirectUrl: process.env.REDIRECT_URL,
-    secret: process.env.SECRET
+  smoochBaseUrl: process.env.SMOOCH_BASE_URL,
+  clientId: process.env.CLIENT_ID,
+  redirectUrl: process.env.REDIRECT_URL,
+  secret: process.env.SECRET
 }
 
-const development = {
-    smoochBaseUrl: 'http://localhost:8091',
-    clientId: 'shoplifter',
-    redirectUrl: 'http://localhost:3000/oauth',
-    secret: 'secret'
+try {
+  config = _.defaults(config, require('./config/config.json'));
+} catch (e) {
+  // do nothing
 }
 
-const production = {
-    smoochBaseUrl: 'https://app.smooch.io',
-    clientId: 'shoplifter',
-    redirectUrl: 'https://shoplifter.herokuapp.com/oauth'
-}
+config = _.defaults(config, require('./config/default/config.json'));
 
-if (process.env.NODE_ENV === 'production') {
-    config = _.defaults(config, production);
-} else {
-    config = _.defaults(config, development);
-}
+console.log('Server Configuration:\n', config);
 
-if (!config.secret) {
-    throw new Error('Missing environment variable: SECRET');
+if (!config.secret || !config.redirectUrl || !config.clientId) {
+  throw new Error('Missing environment variable. Please ensure the following are set: CLIENT_ID, SECRET, REDIRECT_URL');
 }
 
 module.exports = config;

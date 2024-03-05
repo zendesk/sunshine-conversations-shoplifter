536710fa0141cdc406c9bb74b52d2788fd661fd6
dannytranlx <datran@zendesk.com>
2024-03-05 10:56:07 -0500

    Add formatting

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ let config = {
   smoochBaseUrl: process.env.SMOOCH_BASE_URL,
   clientId: process.env.CLIENT_ID,
   redirectUrl: process.env.REDIRECT_URL,
-  secret: process.env.SECRET
-}
+  secret: process.env.SECRET,
+};
 
 try {
   config = _.defaults(config, require('./config/config.json'));
@@ -18,7 +18,9 @@ config = _.defaults(config, require('./config/default/config.json'));
 console.log('Server Configuration:\n', config);
 
 if (!config.secret || !config.redirectUrl || !config.clientId) {
-  throw new Error('Missing environment variable. Please ensure the following are set: CLIENT_ID, SECRET, REDIRECT_URL');
+  throw new Error(
+    'Missing environment variable. Please ensure the following are set: CLIENT_ID, SECRET, REDIRECT_URL'
+  );
 }
 
 module.exports = config;

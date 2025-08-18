37f523a532e87d91284482840cbe970ab9c8c440
Andrew Lavers <alavers@zendesk.com>
2025-08-18 16:43:43 -0400

    Rewrite api interactions and use tokeninfo endpoint

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
+const zendeskDomain = process.env.ZENDESK_DOMAIN || 'zendesk.com';
 const config = {
-  ...require('./config/default/config.json'),
-  suncoBaseUrl: process.env.SUNCO_BASE_URL,
   oauthClientId: process.env.OAUTH_CLIENT_ID,
   oauthClientSecret: process.env.OAUTH_CLIENT_SECRET,
+  zendeskDomain,
+  oauthBaseUrl: `https://oauth-bridge.${zendeskDomain}/sc`,
 };
 
-if (
-  !config.suncoBaseUrl ||
-  !config.oauthClientSecret ||
-  !config.oauthClientId
-) {
+if (!config.oauthClientId || !config.oauthClientSecret) {
   for (const key in config) {
     console.log(key, typeof config[key]);
   }
   throw new Error(
-    'Missing environment variable. Please ensure the following are set: SUNCO_BASE_URL, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_REDIRECT_URL',
+    'Missing environment variable. Please ensure the following are set: OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET',
   );
 }
 
--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# A simple run script for local development.
-
-# The base URL for Sunshine Conversations
-export SUNCO_BASE_URL="https://app.smooch.io"
-
 # The clientID and secret from apps.zendesk.com
 # https://developer.zendesk.com/documentation/marketplace/building-a-marketplace-bot/request-bot-oauth-credentials/
 export OAUTH_CLIENT_ID="your_bot_here"
 export OAUTH_CLIENT_SECRET="secret"
-
-# Override the default port value (3000) here if you need to.
+export ZENDESK_DOMAIN="zendesk.com"
 export PORT=3000
 
 node server.js
